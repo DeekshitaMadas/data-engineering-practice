6e8b255abd8480ba46ab7079411d60ac034f5784
Deekshita Madas <38306654+DeekshitaMadas@users.noreply.github.com>
2023-02-28 00:50:00 -0500

    Update Solution_step3.py

--- Exercises/Exercise-7/Solution_step3.py
+++ Exercises/Exercise-7/Solution_step3.py
@@ -19,3 +19,22 @@ df = df.withColumn("brand", when(df.model.startswith("HGST"), "HGST")
                              .when(df.model.startswith("TOSHIBA"), "Toshiba")
                              .when(df.model.startswith("WDC"), "Western Digital")
                              .otherwise("Other"))
+
+##aternate brand solution
+# Add a new column called brand
+df = df.withColumn("brand", when(split(df.model, "\s+").getItem(0).isNull(), "unknown").otherwise(split(df.model, "\s+").getItem(0)))
+
+# Inspect a column called capacity_bytes. Create a secondary DataFrame that relates capacity_bytes to the model column, create "buckets" / "rankings" for those models with the most capacity to the least. Bring back that data as a column called storage_ranking into the main dataset.
+capacity_df = df.select("model", "capacity_bytes").distinct().groupBy("model").max("capacity_bytes").orderBy("max(capacity_bytes)", ascending=False)
+capacity_df = capacity_df.withColumn("storage_ranking", coalesce(capacity_df.ranking(), capacity_df.count()))
+df = df.join(capacity_df.select("model", "storage_ranking"), "model", "left")
+
+# Create a column called primary_key that is hash of columns that make a record unique in this dataset.
+df = df.withColumn("primary_key", hash(df.survey_date, df.serial_number, df.model))
+
+# Show the final DataFrame
+df.show()
+
+
+
+
